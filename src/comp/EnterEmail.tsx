5171562cadadb25f539aefc9a9fafa61bae5ad5e
Yoni Water Man <yonixw@gmail.com>
2022-03-28 19:06:20 +0000

    Main form

--- src/comp/EnterEmail.tsx
+++ src/comp/EnterEmail.tsx
@@ -10,15 +10,11 @@ import {
   Input,
   VStack,
   useColorModeValue,
+  Center,
 } from "@chakra-ui/react";
 
 export function EnterEmail() {
   const [email, setEmail] = React.useState("");
-  const [pass, setPass] = React.useState("");
-  const [remember, setRemember] = React.useState(false);
-
-  //https://chakra-ui.com/docs/styled-system/theming/theme
-  //const bg_color = useColorModeValue("primary", "black.200"); // useColorModeValue("white.500", "black.200");
 
   const formSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
@@ -26,8 +22,6 @@ export function EnterEmail() {
       JSON.stringify(
         {
           email,
-          pass,
-          remember,
         },
         null,
         4
@@ -37,45 +31,38 @@ export function EnterEmail() {
 
   return (
     /*  <Flex bg="gray.100" align="center" justify="center" h="100vh"> */
-    <Box bg={"aaa"} p={6} rounded="md">
-      <form onSubmit={formSubmit}>
-        <VStack spacing={4} align="flex-start">
-          <FormControl>
-            <FormLabel htmlFor="email">Email Address</FormLabel>
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              variant="filled"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              variant="filled"
-              onChange={(e) => setPass(e.target.value)}
-              value={pass}
-            />
-          </FormControl>
-          <Checkbox
-            id="rememberMe"
-            name="rememberMe"
-            onChange={(e) => setRemember(e.target.checked)}
-            isChecked={remember}
-            colorScheme="purple"
-          >
-            Remember me?
-          </Checkbox>
-          <Button type="submit" colorScheme="purple" isFullWidth>
-            Login
-          </Button>
-        </VStack>
-      </form>
+    <Box bg="boxbg" p={6} rounded="md">
+      <VStack spacing={4} align="flex-start">
+        <form onSubmit={formSubmit} style={{ width: "100%" }}>
+          <VStack spacing={4} align="flex-start">
+            <FormControl>
+              <FormLabel htmlFor="email">Email Address</FormLabel>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                variant="filled"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </FormControl>
+            <Button type="submit" colorScheme="brand" isFullWidth>
+              Login
+            </Button>
+          </VStack>
+        </form>
+        <Center
+          style={{ width: "100%" }}
+          fontSize="md"
+          fontWeight="medium"
+          color="gray.800"
+        >
+          Or
+        </Center>
+        <Button colorScheme="brand" isFullWidth>
+          Generate New
+        </Button>
+      </VStack>
     </Box>
     /* </Flex> */
   );
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,13 +1,29 @@
 import { extendTheme, useColorModeValue } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 
+// https://chakra-ui.com/docs/styled-system/theming/theme
+// https://www.quackit.com/html/codes/color/color-tester.cfm?colorname=tomato
+
 export const UltraTheme = extendTheme({
+  colors: {
+    brand: {
+      50: "sandybrown",
+      100: "sandybrown",
+      200: "coral",
+      300: "coral",
+      400: "tomato",
+      500: "tomato",
+      600: "tomato",
+      700: "orangered",
+      800: "orangered",
+      900: "orangered",
+    },
+  },
   semanticTokens: {
     colors: {
-      error: "red.500",
-      aaa: {
-        default: "#0f0",
-        _dark: "#f0f",
+      boxbg: {
+        default: "white.700",
+        _dark: "grey.700",
       },
     },
   },
